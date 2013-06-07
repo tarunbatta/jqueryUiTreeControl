c23f128e2635349b5fedfb479e088258cf4fc82a
Tarun Batta <tarunbatta@gmail.com>
2013-06-07 14:02:41 -0700

    updated reference js

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,3 +1,3 @@
 ﻿/// <reference path="~/Scripts/jquery-2.0.1.min.js" />
 /// <reference path="~/Scripts/jquery-ui-1.10.3.custom.min.js" />
-/// <reference path="~/Scripts/jquery.btechco.tree" />
+/// <reference path="~/Scripts/jquery.btechco.tree.js" />
